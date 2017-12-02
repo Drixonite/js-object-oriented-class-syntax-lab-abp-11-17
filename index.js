b74927dc37fb32b9bc9dc31ea7b31d3ec6fab299
David Kim <mail.ddk@gmail.com>
2017-12-02 23:56:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+class boardMembers {
+  BoardMember (name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+}
+
+  this.prototype.veto(){
+    return "No, I must disagree"
+  }
+
+}
+
+
+
+class Ceo extends BoardMember {
+  hireEmployee() {
+    return "Welcome aboard!"
+  }
+
+}

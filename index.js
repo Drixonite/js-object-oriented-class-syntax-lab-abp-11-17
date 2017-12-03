cbbacd8d3122c91126dc7e35dfcc4cc92d032022
David Kim <mail.ddk@gmail.com>
2017-12-03 01:02:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class BoardMember {
   constructor(name, homeState, training){
 
     this.name = name;
-    this.homestate = homestate;
+    this.homeState = homeState;
     this.training = training
 
 

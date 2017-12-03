e90eb104f04c37b898dc1d9cf5a298af0e02a4de
David Kim <mail.ddk@gmail.com>
2017-12-03 00:58:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class BoardMember {
   releasePressStatement(){
     return "You will see great things from Scuber."
   }
-  sayHello(){
+  sayHi(){
     `Hi, my name is ${name}. I am from ` + homeState + ", and I was trained in " + training + `.`
   }
 }

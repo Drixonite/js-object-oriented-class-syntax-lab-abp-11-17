447f7908eb68474d9f60a1cc4504c4d6cd8e27fa
David Kim <mail.ddk@gmail.com>
2017-12-03 01:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 class BoardMember {
   constructor(name, homeState, training){
-    this.name = name;
     this.homeState = homeState;
     this.training = training;
+    this.name = name;
+
   }
   veto(){
     return "No, I must disagree"
